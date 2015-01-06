9131024aef962521c41b01140cdc273a72875c44
Alexander Shvets <alexander.shvets@outlook.com>
2015-01-06 17:10:46 +0200

    Remove not used file

--- src/Cataloguer.Web/App_Start/BundleConfig.cs
+++ src/Cataloguer.Web/App_Start/BundleConfig.cs
@@ -36,7 +36,7 @@ namespace Cataloguer.Web
                 "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                 "~/Content/bootstrap.css",
+                 "~/Content/bootstrap/bootstrap.css",
                  "~/Content/Site.css"));
         }
     }
